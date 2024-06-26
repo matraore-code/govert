17bc37d6f0a45a484a05ee949340e4343f900fca
Robert Bright <robright28@gmail.com>
2024-06-26 08:39:55 +0100

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'login_page.dart';
 import 'home_page.dart';
 
-void main() {
-  runApp(GovertApp());
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'postgresql://postgres.yxvbvtgtrxsgsmirrfmp:[YOUR-PASSWORD]@aws-0-eu-west-1.pooler.supabase.com:6543/postgres',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4dmJ2dGd0cnhzZ3NtaXJyZm1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4Mzg4ODAsImV4cCI6MjAzMzQxNDg4MH0.OoMKpVw_yxijEf9Kz4-hZv-DxwTcI9837yOWnsQreho',
+  );
+            
+  runApp(GovertApp()) ;
 }
 
+// Get a reference your Supabase client
+final supabase = Supabase.instance.client;
 class GovertApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -18,3 +26,4 @@ class GovertApp extends StatelessWidget {
     );
   }
 }
+
